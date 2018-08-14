adfe09031892636ffcc563ac95778f292da38820
Hayk Khachatryan <kh.ha.hayk@gmail.com>
2018-08-14 10:40:34 +0400

    fixed ordering of finalCircuit = finalCircuit * multiplyer

--- include/classes/circuit.h
+++ include/classes/circuit.h
@@ -33,8 +33,6 @@ class Circuit
 
         void setCircuit(const std::vector<std::pair<math::Matrix, std::vector<math::Ket*>>> circ) {
             circuit = circ;
-            std::cout << "nono" << std::endl;
-
             circuitInitializer();
         };
 
@@ -43,57 +41,34 @@ class Circuit
             math::Matrix multiplyer = {{1}};
             std::vector<bool> range;
 
-            std::cout << "test?" << std::endl;
             for (auto i : circuit) {
                 range = std::vector<bool>(qubits.size());
                 multiplyer = {{1}};
-                std::cout << "Gate:" << std::endl;
-                i.first.print();
 
                 if (i.first.getXSize() != std::pow(2, i.second.size())) {
                     throw std::runtime_error("Invalid number of qubits to apply gate to");
                 };
 
                 if (i.second == qubits) {
-                    finalCircuit *= i.first;
+                    finalCircuit = i.first * finalCircuit;
                 } else {
-                    std::cout << "qubits:" << std::endl;
                     for (auto j : i.second) {
-                        j->print();
                         range[objectFinder(j)] = true;
                     };
                 };
-                std::cout << "before matrix for loop\nrange:\n";
-                for (auto i : range) {
-                    std::cout << i << std::endl;
-                }
                 for (int m = 0; m < range.size(); m++) {
                     if (range[m] == 1) {
-                        std::cout << "yes range\n";
                         multiplyer = tensorProduct(multiplyer, i.first);
-                        multiplyer.print();
                     } else {
-                        std::cout << "no range\n";
                         multiplyer = tensorProduct(multiplyer, gates::I);
-                        multiplyer.print();
                     };
                 };
 
-                std::cout << "after matrix for loop\n";
-
-
+                std::cout << "Gate: " << std::endl;
+                i.first.print();
                 multiplyer.print();
-                finalCircuit = multiplyer * finalCircuit;
-
-                std::cout << "qaqa" << std::endl;
-
+                finalCircuit  = finalCircuit * multiplyer;
             };
-
-            finalCircuit.print();
-
-
-
-
         };
 
         int objectFinder(math::Ket *qub)
