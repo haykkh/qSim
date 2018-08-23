d821926f60eba97dcef392d061dd3a8ea4a57aac
Hayk Khachatryan <kh.ha.hayk@gmail.com>
2018-08-23 17:38:58 +0400

    reversecontrollers
    implemented two control qubits
    still need to flesh it out and generalise it for some cases

--- test/reversecontrol.cpp
+++ test/reversecontrol.cpp
@@ -13,47 +13,137 @@ Matrix controller(Matrix U, int ctrl, int targ, int size) {
     };
 };
 
+Matrix controllerAgain(Matrix U, int ctrl, int targ, int size) {
+    Matrix Z = gates::Z;
+    if (ctrl < targ) {
+        if (targ - ctrl - 2 < 0) {
+            // figure out adding one, three, and five
+            Matrix two = U - In(2);
+            Matrix four = In(1) - Z;
+            return In(size) + tensorProduct(four, two) / 2;
+        } else {
+            Matrix one = In(size - targ);
+            Matrix two = U - In(2);
+            Matrix three = In(targ - ctrl - 2);
+            Matrix four = In(1) - Z;
+            Matrix five = In(ctrl - 1);
+
+            return In(size) + tensorProduct(five, tensorProduct(four, tensorProduct (three, tensorProduct(two, one)))) / 2;
+        }
+    } else {
+        if (ctrl - targ - 2 < 0){
+            // figure out adding one, three, and five
+            Matrix two = In(1) - Z;
+            Matrix four = U - In(2);
+            return In(size) + tensorProduct(four, two) / 2;
+        } else {
+            Matrix one = In(size - targ);
+            Matrix two = In(1) - Z;
+            Matrix three = In(ctrl - 2 - targ);
+            Matrix four = U - In(2);
+            Matrix five = In(targ - 1);
+
+            return In(size) + tensorProduct(five, tensorProduct(four, tensorProduct(three, tensorProduct(two, one)))) / 2;
+        }
+
+   };
+};
+
 
 int main() {
-    Ket k0 = states::z0;
-    Ket k1 = states::z1;
-
-    Ket* q0 = &k0;
-    Ket* q1 = &k1;
-
     Matrix X = gates::X;
     Matrix Z = gates::Z;
     Matrix I = gates::I;
 
-    Matrix i2 = tensorProduct(I, I);
+    //cout << "one\n";
+    Matrix one = controller(X, 1, 2, 2);
+    //one.print();
+//
+    //cout << "two\n";
+    Matrix two = controller(X, 2, 1, 2);
+    //two.print();
+//
+    //cout << "3\n";
+    //Matrix three = controller(X, 2, 3, 3);
+    //three.print();
+//
+    //cout << "4\n";
+    //Matrix four = controller(X, 2, 1, 3);
+    //four.print();
+//
+    //cout << "5\n";
+    //Matrix five = controller(X, 3, 1, 3);
+    //five.print();
+//
+    //cout << "6\n";
+    //Matrix six = controller(X, 1, 2, 3);
+    //six.print();
+//
+    //cout << "7\n";
+    //Matrix seven = controller(X, 3, 2, 3);
+    //seven.print();
+//
+    //cout << "8\n";
+    Matrix eight = controller(X, 1, 3, 3);
+    //eight.print();
+//
+    //cout << "9\n";
+    //Matrix nine = controllerAgain(one, 1, 3, 3);
+    //nine.print();
+//
+    //cout << "10\n";
+    //Matrix ten = controllerAgain(two, 3, 1, 3);
+    //ten.print();
+//
+    cout << "11\n";
+    Matrix eleven = controllerAgain(two, 1, 2, 4);
+    eleven.print();
+//
 
-    Matrix uid = X - I;
-    Matrix idz = I - Z;
-
-    Matrix uz = tensorProduct(idz, uid)/2;
-
-    Matrix fc = i2 + uz;
-
-    i2.print();
-    uid.print();
-    idz.print();
-    uz.print();
-
-    fc.print();
+    cout << "11.5\n";
+    Matrix elevenfive = controllerAgain(one, 3, 2, 3);
+    elevenfive.print();
 
 
-    Matrix gg = In(0);
-
-    gg.print();
-
-    Matrix CX = controller(X, 1, 2, 2);
-    Matrix XC = controller(X, 2, 1, 2);
-
-    Matrix nCX = controller(X, 3, 1, 3); 
-
-    CX.print();
-    XC.print();
-
-    nCX.print();
+    //cout << "12\n";
+    //Matrix twelve = controllerAgain(two, 2, 4, 4);
+    //twelve.print();
+//
+    //cout << "13\n";
+    //Matrix thirteen = controllerAgain(one, 1, 3, 4);
+    //thirteen.print();
+//
+    //cout << "14\n";
+    //Matrix fourteen =  controllerAgain(one, 1, 4, 4);
+    //fourteen.print();
+//
+    //cout << "15\n";
+    //Matrix fifteen = controllerAgain(eight, 1, 4, 4);
+    //fifteen.print();
+//
+    //cout << "16\n";
+    //Matrix sixteen = controllerAgain(one, 1, 4, 4);
+    //sixteen.print();
+//
+    //cout << "17\n";
+    ////Matrix seventeen = controllerAgain(five, 4, 1, 4);
+    ////seventeen.print();
+//
+    //cout << "18\n";
+    //Matrix eighteen = controllerAgain(two, 4, 1, 4);
+    //eighteen.print();
+//
+    //cout << "19\n";
+    //Matrix nineteen = controllerAgain(two, 3, 1, 4);
+    //nineteen.print();
+//
+    //cout << "20\n";
+    //Matrix twenty = controller(X, 2, 3, 4);
+    //twenty.print();
+//
+//
+    //cout << "21\n";
+    //Matrix twentyone = controller(X, 3, 2, 4);
+    //twentyone.print();
 
 }
