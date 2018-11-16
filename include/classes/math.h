e03c44b4570a7476634699c47a26d1db688ce1c9
Hayk Khachatryan <kh.ha.hayk@gmail.com>
2018-11-16 22:47:30 +0000

    Commenting math.h

--- include/classes/math.h
+++ include/classes/math.h
@@ -28,25 +28,54 @@
 namespace qsim {
     namespace math {
 
+        // imaginary number
         extern const std::complex<double> I(0,1);
+        
+        // π
         extern const double pi = std::atan(1) * 4;
 
+        // exp[2πi / 2^n]
         std::complex<double> omega(int n) {
             return std::exp((2 * pi * I) / std::pow(2, n));
         };
 
+        /*
+         *  Returns:
+         *          ""  if val > 0
+         *          "-" if val < 0
+         */
         template <typename T> const char* sgn(T val) {
             return (val > 0) ? "" : "-";
         };
 
         class Matrix {
+            /*
+             *  Used as a representation for gates and circuits
+             *  an n-qubit gate is a matrix of size 2^n x 2^n
+             */ 
+
+
             private:
+
+                // data of matrix 
                 std::vector<std::vector<std::complex<double>>> data;
+                
+                // Number of row elements
                 std::vector<std::vector<std::complex<double>>>::size_type ySize;
+                
+                // Number of column elements
                 std::vector<std::complex<double>>::size_type xSize;
+                
+                // indicating whether this gate controls another qubit
                 bool control = false;
+
+                // indicating whether this gate is controlled by another qubit
                 bool controlledGate = false;
+                
+                // typedef for a shared_ptr to a matrix 
                 typedef std::shared_ptr<Matrix> Matrix_ptr;
+
+                // pointer to the gate that is controlled by this gate
                 Matrix_ptr gateControlled;
                 
             public:
@@ -54,82 +83,107 @@ namespace qsim {
 
                 Matrix() {};
 
-
+                /*
+                 *  Init matrix with:
+                 *          data = d
+                 *          ySize = number of rows in d
+                 *          xSize = number of columns in d
+                 */ 
                 Matrix(std::initializer_list<std::vector <std::complex <double> > > d) : data(d) {
                     ySize = data.size();
                     xSize = data[0].size();
                     
                 };
 
+                // Init matrix of size m x n
                 Matrix(unsigned int n, unsigned int m) {
                     setSize(n,m);
                 };
+
+                // Init square matrix of size n x n
                 Matrix(unsigned int n) {
                     setSize(n,n);
                 };
 
+                // Set size of matrix to m x n
                 void setSize(unsigned int n, unsigned int m) {
                     xSize = n;
                     ySize = m;
                     data.resize(ySize, std::vector<std::complex <double> >(xSize));
                 };
                 
+                // Set value at (x,y) to value
                 void setValue(unsigned int y, unsigned int x, const std::complex<double> value) {
                     data[y][x] = value;
                 };
 
+                // Set whether this gate is controlled by another qubit
                 void setControlled(bool val) {
                     controlledGate = val;
                 };
 
+                // Set data = d and resize
                 void setData(std::vector<std::vector<std::complex<double>>> d) {
                     data = d;
                     ySize = data.size();
                     xSize = data[0].size();
                 };
 
+                // Specify pointer of gate that is controlled by this gate
                 void setGateControlled(const Matrix_ptr mat)
                 {
                     gateControlled = mat;
                 };
 
+                // Set whether this gate controls another qubit
                 void setControlGate(bool val) {
                     control = val;
                 };
 
+                // Returns whether this gate controls another qubit
                 bool isControlGate() {
                     return control;
                 };
 
+                // Returns whether this gate is controlled by another qubit
                 bool isControlled()
                 {
                     return controlledGate;
                 };
 
+                // Returns number of rows of matrix
                 std::vector<std::vector<std::complex<double>>>::size_type getYSize() const {
                     return ySize;
                 };
 
+                // Returns number of columns of matrix
                 std::vector<std::complex<double>>::size_type getXSize() const {
                     return xSize;
                 };
                 
+                // Returns a vector<vector>> list of data
                 std::vector<std::vector<std::complex<double>>> getData() const {
                     return data;
                 };
 
+                // returns the pointer to the gate that is controlled by this gate
                 Matrix getGateControlled() const {
                     return *gateControlled;
                 };
 
+                // get the value of this matrix at (x,y)
                 std::complex<double> getValue(unsigned int y, unsigned int x) const
                 {
                     return data[y][x];
                 };
 
+                // returns this gate controlled 
                 Matrix controlled() const
                 {
+                    // init matrix twice the size of *this
                     Matrix res(xSize * 2, ySize * 2);
+
+                    // make the top left quadrant an identity matrix
                     for (int j = 0; j < ySize; j++)
                     {
                         res.setValue(j, j, 1);
@@ -139,11 +193,17 @@ namespace qsim {
                             res.setValue(ySize + j, xSize + i, getValue(j, i));
                         };
                     };
+
+                    // set the gate that is being controlled
                     res.setGateControlled(std::make_shared<Matrix>(*this));
+
+                    // set this gate to be controlled
                     res.setControlled(true);
+
                     return res;
                 };
 
+                // prints the matrix
                 void print() {
                     for (int j = 0; j < ySize; j++)
                     {
@@ -183,6 +243,7 @@ namespace qsim {
                     std::cout << std::endl;
                 };
 
+                // prints all non-zero elements
                 void noZeroPrint() {
                     for (int j = 0; j < ySize; j++)
                     {
@@ -226,6 +287,7 @@ namespace qsim {
                     std::cout << std::endl;
                 };
 
+                // prints an box array (█ for all non-zero elements)
                 void arrayPrint() {
                     for (int j = 0; j < ySize; j++)
                     {
@@ -247,6 +309,7 @@ namespace qsim {
                     std::cout << std::endl;
                 };
 
+                // returns the adjoint of *this matrix
                 Matrix adjoint() {
                     Matrix res(xSize, ySize);
 
@@ -259,6 +322,7 @@ namespace qsim {
                     return res;
                 };
 
+                // Subtract matrices
                 Matrix operator-=(Matrix const &mat) {
                     Matrix res(xSize, ySize);
                     if (xSize != mat.getXSize() || ySize != mat.getYSize()) {
@@ -274,6 +338,7 @@ namespace qsim {
                     return *this;
                 };
 
+                // Add matrices
                 Matrix operator+=(Matrix const &mat) {
                     Matrix res(xSize, ySize);
                     if (xSize != mat.getXSize() || ySize != mat.getYSize()) {
@@ -289,7 +354,7 @@ namespace qsim {
                     return *this;
                 };
 
-
+                // Multiply matrices
                 Matrix operator*=(Matrix const &mat) {
                     Matrix res(xSize, ySize);
                     std::complex<double> midresult = 0;
@@ -318,6 +383,7 @@ namespace qsim {
                     return *this;
                 };
 
+                // Multiply matrix by a number
                 Matrix operator*=(std::complex<double> multiplier) {
                     Matrix res(xSize, ySize);
                     for (int j = 0; j < ySize; j++) {
@@ -329,6 +395,7 @@ namespace qsim {
                     return *this;
                 };
 
+                // Divice matrix by a number
                 Matrix operator/=(std::complex<double> divider) {
                     Matrix res(xSize, ySize);
                     for (int j = 0; j < ySize; j++) {
@@ -341,22 +408,27 @@ namespace qsim {
                 };
         };
 
+        // Multiply matrices
         Matrix operator*(Matrix mat1, Matrix mat2) {
             return mat1 *= mat2;
         };
 
+        // Subtract matrices
         Matrix operator-(Matrix mat1, Matrix mat2) {
             return mat1 -= mat2;
         };
 
+        // Add matrices
         Matrix operator+(Matrix mat1, Matrix mat2) {
             return mat1 += mat2;
         };
 
+        // Multiply matrix by number
         Matrix operator*(Matrix mat1, std::complex<double> multiplier) {
             return mat1 *= multiplier;
         };
 
+        // Divice matrix by number
         Matrix operator/(Matrix mat1, std::complex<double> divider){
             return mat1 /= divider;
         };
@@ -377,6 +449,7 @@ namespace qsim {
             return res;
         }
 
+        // not in use
         Matrix multiplyerApplicator(Matrix multiplyer, Matrix gate, Matrix identity, std::vector<char> range)
         {
             Matrix res = multiplyer;
@@ -395,6 +468,7 @@ namespace qsim {
             return res;
         };
 
+        // Returns identity matrix of size 2^n x 2^n
         Matrix In(unsigned int n) {
             Matrix res(std::pow(2, n));
 
@@ -408,9 +482,20 @@ namespace qsim {
 
         class Ket
         {
+            /*
+             *  Ket vector (column vector in Dirac (bra-ket) notation)
+             *  Used as a representation for qubits
+             *  an n qubit system is a vector of size 2^n
+             */ 
+
             private:
+                // data of vector
                 std::vector<std::complex<double>> data;
+
+                // size of vector
                 std::vector<std::complex<double>>::size_type size;
+
+                // the state of a qubit in binary form (eg '1', '0', '101', '010011' etc)
                 std::vector<int> state;
 
             public:
@@ -418,30 +503,40 @@ namespace qsim {
 
                 };
 
+                /*
+                 * Init ket with:
+                 *          data = d
+                 *          size = number of elements in d
+                 */
                 Ket(std::initializer_list<std::complex<double>> d) : data(d)
                 {
                     size = d.size();
                 };
 
+                // Init ket of size n
                 Ket(unsigned int n)
                 {
                     setSize(n);
                 };
 
+                // Set size of ket to n
                 void setSize(unsigned int n)
                 {
                     size = n;
                     data.resize(n);
                 };
 
+                // Set value at index to value
                 void setValue(unsigned int index, const std::complex<double> value) {
                     data[index] = value;
                 }
 
+                // add value as the last element of ket
                 void push_back(std::complex<double> value) {
                     data.insert(data.end(), value);
                 };
 
+                //Populates state in binary form based on values in data 
                 void initState() {
                     int index = distance(data.begin(), find(data.begin(), data.end(), 1.0)) + 1;
                     
@@ -460,12 +555,14 @@ namespace qsim {
                     };
                 };
 
+                // returns binary form of state
                 std::vector<int> getState() {
                     initState();
 
                     return state;
                 };
 
+                // prints binary form of state (eg '1', '0', '101', '010011' etc) 
                 void printState() {
                     std::cout << std::endl;
                     for (auto i : state) {
@@ -474,22 +571,51 @@ namespace qsim {
                     std::cout << std::endl;
                 };
 
-
+                // get value at index
                 std::complex<double> getValue(unsigned int index) const {
                     return data[index];
                 }
 
+                // get entire vector
                 std::vector<std::complex<double>> getData() const {
                     return data;
                 }
 
+                // get number of elements in vector
                 std::vector<std::complex<double>>::size_type getSize() const {
                     return size;
                 };
 
 
+                // Simulates measuring the qubit 
+                /*
+                 *  p: vector of cumulative probabilities
+                 *      eg for data = ( 0.5,    0, 0.5,   0,  0.5,    0, 0.5,   0)
+                 *                p = (0.25, 0.25, 0.5, 0.5, 0.75, 0.75, 1.0, 1.0) 
+                 * 
+                 *  then generates a random number, rnd, in (0,1)
+                 *  
+                 *  then check where in p rnd falls and sets that to the var qub
+                 *      eg for rnd = 0.37
+                 *         it falls between the second and third element
+                 *         => third element is 1
+                 *         => qub = 2
+                 *         => qubit is in state 00100000
+                 * 
+                 *         for rnd = 0.02
+                 *         before 1st element
+                 *         => 1st element is 1
+                 *         => qub = 0
+                 *         => qubit in state 10000000
+                 * 
+                 *  When it has figured out the state
+                 *  it resets this qubit to all zero elements
+                 *  then sets the value at the qub index to 1
+                 * 
+                 */ 
                 void measure(){
-                    std::vector<double> p;
+
+                    std::vector<double> p; 
                     double summer;
 
                     for (auto q : data) {
@@ -510,6 +636,7 @@ namespace qsim {
 
                 }
 
+                // prints qubit
                 void print()
                 {
                     std::cout << std::endl;
@@ -676,6 +803,7 @@ namespace qsim {
             return ket *= mat;
         };
 
+        
         typedef std::shared_ptr<Ket> Ket_ptr;
         typedef std::shared_ptr<Matrix> Matrix_ptr;
 
