18ffdace4511ff38af7e783621a88349712bad39
Hayk Khachatryan <kh.ha.hayk@gmail.com>
2018-11-16 23:20:07 +0000

    removed test files

--- test/circuitprint.cpp
+++
@@ -1,9 +0,0 @@
-#include "../include/qSim.h"
-
-using namespace std;
-using namespace qsim;
-using namespace math;
-
-int main() {
-    std::cout << "--◼︎--|Z|------●--\n  │           │\n--◼︎---◼︎--|H|--●--\n      │       │\n-|X|--◼︎------|X|-";
-}
